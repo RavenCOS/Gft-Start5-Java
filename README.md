47bfb5c9f517732fb69dd38363a084a22cf09e20
Marcos Castro <marcoscastro650@gmail.com>
2022-05-14 14:27:42 -0300

    FirstCommit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Primeiro desafio de projeto Git/Github da dio
+[GFT Start #5 Java](https://web.dio.me/track/gft-start-5-java)
+
+## Links Importantes: 
+[GIT](https://git-scm.com/)
+
+[OpenJDK ZULU](https://www.azul.com/downloads/?package=jdk)
+
+### IDE utilizada: 
+[IntelliJ](https://www.jetbrains.com/idea/)
---
+++ src/br/com/dio/bootcamp/declarandoVariaveis.java
@@ -0,0 +1,42 @@
+package br.com.dio.bootcamp;
+
+public class declarandoVariaveis {
+    public static void main(String[] args) {
+        //Exemplo de Boas Práticas:
+        int i;
+        int I; //Sendo uma linguagem case sensitive, podemos usar duas variáveis de mesmo nome, diferenciando com letra maiuscula e minuscula.
+        int numero; //Usamos letras minusculas nas variáveis.
+        int idade1Pedro; //Variáveis podem ser misturadas com letras e números.
+        int quantidadeProduto; //Uso do camelCase para variáveis longas.
+        final int QUANTIDADE_GATOS = 10; //Para constantes é ideal usar letras maiusculas e separar sentenças com _.
+
+        i = 10;
+        I = 20;
+        numero = 2022;
+        idade1Pedro = 29;
+        quantidadeProduto = 200;
+
+        /*
+        Exemplo de Más Práticas:
+            - Não dão erro de compilação, mas não são ideais:
+                int _i;
+                int _sala1;
+                int $ANDAR2;
+                int NumeroQuarto;
+                int qtdgt;
+                final int quantidadeGatos;
+            - Erro de compilação:
+                int 1cor;
+                int Numero Quarto;
+                int _i%;
+                int continue;
+            * Variáveis não podem conter números no inicio, ser divididas com espaço, conter caracteres especiais ou funções usadas pela ide(ex for, goto).
+        */
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(numero);
+        System.out.println(idade1Pedro);
+        System.out.println(quantidadeProduto);
+        System.out.println(QUANTIDADE_GATOS);}
+}
