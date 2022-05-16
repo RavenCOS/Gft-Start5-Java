da8212bbb3721bb953a6b718164484cf1ec8063b
Marcos Castro <marcoscastro650@gmail.com>
2022-05-16 19:44:48 -0300

    First Commit

--- src/br/com/dio/Java_Fundamental/declarandoVariaveis.java
+++ src/br/com/dio/Java_Fundamental/declarandoVariaveis.java
@@ -1,4 +1,4 @@
-package br.com.dio.bootcamp;
+package br.com.dio.Java_Fundamental;
 
 public class declarandoVariaveis {
     public static void main(String[] args) {
@@ -7,7 +7,7 @@ public class declarandoVariaveis {
         int I; //Sendo uma linguagem case sensitive, podemos usar duas variáveis de mesmo nome, diferenciando com letra maiuscula e minuscula.
         int numero; //Usamos letras minusculas nas variáveis.
         int idade1Pedro; //Variáveis podem ser misturadas com letras e números.
-        int quantidadeProduto; //Uso do camelCase para variáveis longas.
+        int quantidadeProduto; //Uso do camelCase para variáveis de várias palavras.
         final int QUANTIDADE_GATOS = 10; //Para constantes é ideal usar letras maiusculas e separar sentenças com _.
 
         i = 10;
