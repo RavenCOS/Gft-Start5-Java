6d55fa819caee73d5e697b11fc773c3741e2f4a7
Marcos Castro <marcoscastro650@gmail.com>
2022-05-16 20:38:23 -0300

    Aula 3 de Java fundamental

---
+++ src/br/com/dio/Java_Fundamental/operadoresAritimeticos.java
@@ -0,0 +1,35 @@
+package br.com.dio.Java_Fundamental;
+
+public class operadoresAritimeticos {
+    public static void main(String[] args) {
+        /*pós-fixado: exp++ ou exp--
+        * prefixado: ++exp ou --exp*/
+            int a = 10;
+            System.out.println("Variavel a sem alteracoes: " + a);
+            int b = a--;
+            System.out.println("Variavel b: " + b);
+            System.out.println("Variavel a depois do a--: " + a);
+            int c = 80;
+            System.out.println("Variavel c sem alteracoes: " + c);
+            int d = ++c;
+            System.out.println("Variavel d: " + d);
+            System.out.println("Variavel c depois do ++c: " + c);
+
+        /*Aritimetico*/
+            int e = 50;
+            int f = 10;
+            int g = e - f;
+            System.out.println(g);
+            int h = 10;
+            float i = 3.14F;
+            float j = h + i / g * f;
+            System.out.println(j);
+        /*Atribuiçoes*/
+            e += f + 5;
+            int k = e = --a + 1;
+        System.out.println(k);
+        System.out.println(e);
+        System.out.println(a);
+
+    }
+}
